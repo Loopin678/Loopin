41bf253f72ede6cca8745e225b96ab4bfafe0926
Arnav <arnavalternate619@gmail.com>
2026-08-20 22:21:04 +0530

    Auth Setup Base Code Complete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import "dotenv/config"
 import express from "express"
+import cookieParser from "cookie-parser"
+import authRoutes from "./routes/auth.routes";
 import { prisma } from "./library/prisma"
 
 //dotenv.config();
@@ -7,9 +9,13 @@ import { prisma } from "./library/prisma"
 const app = express();
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.get("/health", (req, res)=>{
     res.json({ok: true});
 });
+
+app.use("/auth", authRoutes);
 
 const port = process.env.PORT || 4000;
 
